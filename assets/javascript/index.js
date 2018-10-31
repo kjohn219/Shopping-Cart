cd130b21e27dd04aecfea187648c94b19632aca1
Katlin <katjohnson926@yahoo.com>
2018-10-31 19:47:24 -0400

    changed some

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,14 +1,27 @@
+//onClick add to grocery list
+$('#milk').on('click', function(){
 
+$('#bread').on('click', function(){
 
+$('#orangeJuice').on('click', function(){
 
+$('#iceCream').on('click', function(){
+
+$('#banana').on('click', function(){
+
+$('#chips').on('click', function(){
+
+$('#grapes').on('click', function(){
+
+$('#eggs').on('click', function(){
 
 // onClick add the contact to array, clear html list, redraw list
 $('#btnAdd').on('click', function(){
     addContact();
-    $('.List').empty();
+    $('.card-text').empty();
 
     for(let i = 0; i < employeeList.length; i++) {
-        $('.List').append(`<li>${employeeList[i].name},${employeeList[i].officeNum},${employeeList[i].phoneNum}</li>`);
+        $('.card-text').append(`<li>${employeeList[i].name},${employeeList[i].officeNum},${employeeList[i].phoneNum}</li>`);
     };
 });
 
