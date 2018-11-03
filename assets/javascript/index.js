25d1c903faf324576e6b39afd751f2db07f85f5b
Katlin <katjohnson926@yahoo.com>
2018-11-03 13:05:34 -0400

    changes to filter button

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,44 +1,83 @@
-//onClick add to grocery list or filter
-$('#milk').on('click', function(){
-
-$('#bread').on('click', function(){
-
-$('#orangeJuice').on('click', function(){
-
-$('#iceCream').on('click', function(){
-
-$('#banana').on('click', function(){
-
-$('#chips').on('click', function(){
-
-$('#grapes').on('click', function(){
-
-$('#eggs').on('click', function(){
-
-$('#dairy').on('click', function(){
-
-$('#junkFood').on('click', function(){
-
-$('#healthySnack').on('click', function(){
 
 //Build the list for the view
-const buildList = function (arrayName){
-    let newList = ''
-    for (let i = 0, i < arrayName.length, i++){
-        newList += '<i> ${arrayName[i].name'
-    }
-    $(".List").append(newList);
+const buildList = function () {
+    $(".List").append('milk');
+}
+//Build the list for the view
+const buildListBread = function () {
+    $(".List").append('bread');
+}
+//Build the list for the view
+const buildListOj = function () {
+    $(".List").append('Orange Juice');
+}
+//Build the list for the view
+const buildListIc = function () {
+    $(".List").append('Ice Cream');
+}
+//Build the list for the view
+const buildListBanana = function () {
+    $(".List").append('Banana');
+}
+//Build the list for the view
+const buildListChips = function () {
+    $(".List").append('Chips');
+}
+//Build the list for the view
+const buildListGrapes = function () {
+    $(".List").append('Grapes');
+}
+//Build the list for the view
+const buildListEggs = function () {
+    $(".List").append('Eggs');
 }
 
-// onClick add the contact to array, clear html list, redraw list
-$('#btnAdd').on('click', function(){
-    addContact();
-    $('.card-text').empty();
+//Build view for Dairy and hide other buttons
+const buildDairy = function () {
+    $('#grapes').hide();
+    $('#banana').hide();
+    $('#bread').hide();
+    $('#chips').hide();
+    $('#orangeJuice').hide();
+    $('#iceCream').hide();
 
-    for(let i = 0; i < employeeList.length; i++) {
-        $('.card-text').append(`<li>${employeeList[i].name},${employeeList[i].officeNum},${employeeList[i].phoneNum}</li>`);
-    };
-});
+}
 
-//runtime
-addContact()
+//Build view for Junk Food and hide other buttons
+const buildJunk = function () {
+    $('#grapes').hide();
+    $('#milk').hide();
+    $('#orangeJuice').hide();
+    $('#banana').hide();
+    $('#eggs').hide();
+    $('#bread').hide();
+}
+    
+//Build view for Snack and hide other buttons
+const buildHealthySnack = function () {
+    $('#chips').hide();
+    $('#iceCream').hide();
+}
+    
+//onClick add to grocery list or filter
+$('#milk').on('click', buildList);
+
+$('#bread').on('click', buildListBread);
+
+$('#orangeJuice').on('click', buildListOj);
+
+$('#iceCream').on('click', buildListIc);
+
+$('#banana').on('click', buildListBanana);
+
+$('#chips').on('click', buildListChips);
+
+$('#grapes').on('click', buildListGrapes);
+
+$('#eggs').on('click', buildListEggs);
+
+$('#dairy').on('click', buildDairy);
+
+$('#junkFood').on('click', buildJunk);
+
+$('#healthySnack').on('click', buildHealthySnack);
